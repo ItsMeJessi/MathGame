43a9dba4ce060e75df241fedae0d9f0eec4a275b
Jessi <jessica.szarek25@gmail.com>
2022-11-21 20:04:25 +0100

    Game frame script
    Responsible for game frame feature

---
+++ Assets/Scripting/UI/GameFrame.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MathGame.UI
+{
+    public class GameFrame : MonoBehaviour
+    {
+        [SerializeField] private FrameAssetHolder _frameAssetHolder;
+        [SerializeField] private Image _frameImageComponent;
+        
+        private void Start()
+        {
+            _frameImageComponent.sprite = _frameAssetHolder.DefaultFrameAsset;
+        }
+    }
+}
