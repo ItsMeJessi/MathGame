3d4d0392b037fc1b1958e5dc7eb3d7510e83964f
Jessi <jessica.szarek25@gmail.com>
2023-01-12 22:16:06 +0100

    Change scene component for game mode scenes

--- Assets/Scripting/UI/ChangeSceneToGameMode.cs
+++ Assets/Scripting/UI/ChangeSceneToGameMode.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeSceneButton : AbstractButton
+public class ChangeSceneToGameMode : AbstractButton
 {
-    [SerializeField] private string _sceneName;
+    [SerializeField] private CurrentGameConfig _currentGameConfig;
     [SerializeField] private AudioClip _audioClip;
     protected override void Click()
     {
         AudioActions.PlaySound(_audioClip);
-        SceneManager.LoadScene(_sceneName);
+        SceneManager.LoadScene(_currentGameConfig.GameMode);
     }
 }
